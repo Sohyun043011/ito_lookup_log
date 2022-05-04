cd23e6a2a0bcb5371238ae5ced7209cd87b8ae36
so1omon <solmin3665@gmail.com>
2022-05-04 15:12:57 +0900

    weekOfMonth 추가 => moment 모듈 설치 필요

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ const { resolve } = require('url');
 var router = express.Router();
 var db=require('mysql2-promise')();
 var db_config=require('../db_config');
+var moment=require('moment');
+
+const weekOfMonth = (m) => m.week() - moment(m).startOf('month').week() + 1;
+// const nowDate = moment(target_day).utc(true);
 
 function getSessionData(req){
   return new Promise((resolve)=>{
@@ -43,18 +47,6 @@ router.get('/login/:emp_id',async function(req, res){
 });
 
 router.get('/main', function(req, res) { //
-  console.log(req.session.data)
-  const data= async()=>{
-    const result=getSessionData(req);
-    result.then(console.log);
-    console.log(result)
-  }
-  // getSessionData(req).then(value => console.log(value));
-
-  if(req&&req.session&&req.session.data){
-    res.render('main',{list:req.session.data[0]})
-  }
-  else res.status(404).send('<p>오류</p>');
   /*
     메인 페이지
     1. 세션 수 확인 후 N개 미만일 때만 페이지 넘겨주기
@@ -62,6 +54,10 @@ router.get('/main', function(req, res) { //
     3. 출퇴근 기록 조회 | 급량비 및 초과근무 기록 조회의 2개의 tab으로 구성
     이후 main page 리턴
   */
+  if(req&&req.session&&req.session.data){ // request, session, session data 유효성 검사
+    res.render('main',{list:req.session.data[0]}) // 세션 정보를 ejs에 보내줌
+  }
+  else res.status(404).send('<p>오류</p>');
 });
 
 
@@ -74,7 +70,7 @@ router.post('/inout',function(req, res){
   if(!req.session.data){
     res.status(404).send('<p>오류</p>');
   }
-  const {emp_id, start_day, end_day}=req.body;
+  const {emp_id, start_day, end_day}=req.body; // 사번, 시작일시, 종료일시
 
   db.configure(db_config['mysql']);
   sql='select EMP_ID, NAME, YMD, WORK_TYPE, FIX1, `INOUT`, PLAN1 from connect.ehr_cal where emp_id=? and ymd>=? and ymd<=?';
@@ -87,13 +83,32 @@ router.post('/inout',function(req, res){
 });
 
 router.get('/overtime',function(req, res){
+  
   /*
     급량비 및 초과근무 기록 조회
     filter 안에 있는 내용(사번, 부서, 연월 정보)을 기반으로 중계DB의 connect.ehr_cal에서 값을 가져오는 쿼리를 실행
     최초에 해당 연월에 해당하는 ehr_cal 데이터 행 수와 날짜를 기준으로 몇주차까지 완전하게 계산가능한지 판별하고
     각 주차별로 초과근무, 급량비 내역 표출 및 월별 합산해서 표출할 수 있는 데이터 set 생성
     이후 res.json으로 리턴
-  */
+  */ 
+  if(!req.session.data){
+    res.status(404).send('<p>오류</p>');
+  }
+  // const {emp_id, start_day, end_day}=req.body;
+  const {emp_id, start_day, end_day}={emp_id:'20200002', start_day:'20220401', end_day:'20220430'}
+
+  db.configure(db_config['mysql']);
+  sql='select EMP_ID, `NAME`, YMD, CAL_OVERTIME, CAL_MEAL from connect.ehr_cal where emp_id=? and ymd>=? and ymd<=?';
+
+  db.query(sql,[emp_id, start_day, end_day]).spread(function(rows){ // 넘겨받은 emp_id로 직원 정보 조회
+    result=JSON.parse(JSON.stringify(rows));
+    for (row in result){
+      result[row]['WEEK']=weekOfMonth(moment(result[row]['YMD']).utc(true));
+      
+    }
+    console.log(result);
+    // res.json(JSON.parse(JSON.stringify(rows)));
+  });
 });
 
 module.exports=router;
