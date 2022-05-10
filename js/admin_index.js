e139db21a9cfa4a5037568e66d07dcfb75e4a147
sohyun <park.sohyun0430@gmail.com>
2022-05-10 16:09:29 +0900

    220510 로그아웃

--- js/admin_index.js
+++ js/admin_index.js
@@ -1,12 +1,84 @@
 document.write("<script src='../js/index_lib.js'></script>");
 $(document).ready(function(){
 
+    //부서 list 출력
+
     // 로그아웃 버튼 
-    $('#logout-button').on('click',function(){});
+    $('#logout-button').click(function(){
+        alert('로그아웃');
+        $.ajax({
+            type:"GET",
+            url:"/admin/logout",
+            success:function(data){
+                console.log("success");
+                location.replace('/users/main');
+            }
+        })
+    })
 
     //조회버튼
     $('#check-sesarch').on('click',function(){
         
     });
+    $.datepicker.setDefaults({
+        dateFormat: 'yy-mm-dd'
+        ,showOtherMonths : true
+        ,changeYear:true
+        ,changeMonth:true
+        ,minDate:"-6M",
+        maxDate:"+0D",
+        prevText: '이전 달',
+        nextText: '다음 달',
+        monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+        dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+        showMonthAfterYear: true,
+        yearSuffix: '년'
+    });
+    $(function() {
+        $("#admin_datepicker1, #admin_datepicker2").datepicker();
+    });
+    var currentYear = (new Date()).getFullYear();
+    var currentMonth = (new Date()).getMonth();
+    var startYear = currentYear-5;
+    var options = {
+            startYear: startYear,
+            finalYear: currentYear,
+            pattern: 'yyyy-mm',
+            monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월']
+
+    };
+    $('#admin_monthpicker1').monthpicker(options);
+    $('#admin_monthpicker2').monthpicker(options);
+
+    //미래 월은 비활성화시키기
+    var months=[];
+    for(var i=currentMonth+2,j=0;i<=12;i++){
+        months[j++]=i;
+    }
+    for(var i=0;i<$('#admin_monthpicker1').length;i++){
+        $($('#admin_monthpicker1')[i]).monthpicker("disableMonths",months);
+        $($('#admin_monthpicker2')[i]).monthpicker("disableMonths",months);
+        $($('#admin_monthpicker1')[i]).monthpicker().bind('monthpicker-change-year',function(e,year){
+            var item = $(e.currentTarget);
+            if(year==currentYear){
+                $(item).monthpicker('disableMonths',months);
+            }
+            else{
+                $(item).monthpicker('disableMonths',[]);
+            }
+        });
+        $($('#admin_monthpicker1')[i]).monthpicker().bind('monthpicker-change-year',function(e,year){
+            var item = $(e.currentTarget);
+            if(year==currentYear){
+                $(item).monthpicker('disableMonths',months);
+            }
+            else{
+                $(item).monthpicker('disableMonths',[]);
+            }
+        });
+    }
 
 });
