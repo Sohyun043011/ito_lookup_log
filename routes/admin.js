534d900139efec26ec85c2015e4ce98d25c8864a
sohyun <park.sohyun0430@gmail.com>
2022-06-09 14:54:54 +0900

    220609 error page 수정 완료

--- routes/admin.js
+++ routes/admin.js
@@ -393,7 +393,7 @@ router.get('/gw/ehr/con/:emp_id',function(req,res){
 
 router.get('/error',function(req,res){ // admin에서 발생하는 모든 에러 케이스 처리
   var error_msg=req.query.msg;
-  res.redirect('error',{error:error_msg});
+  res.render('error',{error:error_msg});
 });
 
 module.exports=router; //이거 안해주면 main.js app(express) 객체에서 라우터 접근 못함
